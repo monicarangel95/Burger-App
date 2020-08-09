82baec08a9b0fb734ba30df4e142f7269b4b22d7
Monica Rangel <monicarangel95@github.com>
2020-08-09 16:46:22 -0700

    Update seeds.db to include three inserts
    Insert queries to populate the `burgers` table with at least three entries.

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Cheese Burger", false);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Veggie Burger", false);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Bacon Burger", false);
+
+SELECT * FROM burger;
