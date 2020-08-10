e7c248e8574200f11e90939fde51c1fa10bf0c01
Monica Rangel <monicarangel95@github.com>
2020-08-09 19:01:43 -0700

    Update burger_controller.js - created router for the app
    `router` for the app, and export the `router` at the end of your file.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,40 @@ var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burger: data
+        };
+        // console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burger", function (req, res) {
+    burger.insertOne([
+        'burger_name'
+    ], [
+        req.body.burger_name
+    ], function (data) {
+        res.redirect('/');
+    });
+});
+
+router.put('/api/burger/:id', function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function (data) {
+        res.redirect('/');
+
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
