8f9699337db5c385c07e516dc2c6a8938a67a16e
Monica Rangel <monicarangel95@github.com>
2020-10-11 19:58:31 -0700

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,73 +1,39 @@
-// Import MySQL connection.
-var connection = require("../config/connection.js");
-function printQuestionMarks(num) {
-    var arr = []
-    for (var i = 0; i < num; i++) {
-        arr.push("?");
-    }
-    return arr.toString();
-}
-// Helper function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
-    var arr = [];
-    // loop -in through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-        var value = ob[key];
-        if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-            if (typeof value === "string" && value.indexOf("") >= 0) {
-                value = "" + value + "";
-            }
-            arr.push(key + "=" + value);
-        }
-    }
-    // translate array of strings to a single comma-separated string
-    return arr.toString();
-}
-// Object for all our SQL statement functions.
+var connection = require("./connection.js");
 var orm = {
-    //selectAll
-    selectAll: function (tableInput, cb) {
-        var queryString = "SELECT * FROM" + tableInput + ";";
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
+    selectAll: function (callback) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function (error, result) {
+            if (error) {
+                console.log("MYSQL SELECT QUERY ERROR: " + error);
             }
-            cb(result);
+            console.log("Successfully selected all data.");
+            console.log(result);
+            callback(result);
         });
     },
-    // insertOne()
-    insertOne: function (table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES ("
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
-        console.log(queryString);
-        connection.query(queryString.vals, function (err, result) {
-            if (err) {
-                throw err;
+    insertOne: function (burgerName, callback) {
+        var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES ('" + burgerName + "', false)";
+        connection.query(queryString, function (error, result) {
+            if (error) {
+                console.log("MYSQL INSERT QUERY ERROR: " + error);
             }
-            cb(result)
+            console.log("New burger created: " + burgerName + "!");
+            callback(result);
         });
     },
-    // updateOne()
-    updateOne: function (table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
-        queryString += "SET ";
-        queryString += objToSql(objColVals);
-        queryString += "WHERE ";
-        queryString += condition;
-        console.log(queryString);
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
+    updateOne: function (burgerStatus, burgerId, callback) {
+        var queryString = "UPDATE burgers SET devoured = " + burgerStatus + " WHERE id = " + burgerId;
+
+        connection.query(queryString, function (error, result) {
+            if (error) {
+                console.log("MYSQL UPDATE QUERY ERROR: " + error);
+            } else if (result.affectedRows === 0) {
+                console.log("MYSQL UPDATE QUERY ERROR: " + error);
+            } else {
+                console.log("Status of burger has been updated!");
             }
-            cb(result);
+            callback(result);
         });
     }
-};
-    
+}
 module.exports = orm;
